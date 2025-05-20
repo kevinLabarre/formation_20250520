a67ab6c8d59b6bf59c34fbc962eedaa38af82a22
klabarre <klabarre@dawan.fr>
2025-05-20 13:21:33 +0200

    ajout espace admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,39 +1,29 @@
 import { Routes } from '@angular/router';
-import { AccountComponent } from './public/components/account/account.component';
-import { AccountResumeComponent } from './public/components/account-resume/account-resume.component';
-import { RetirerComponent } from './public/components/retirer/retirer.component';
-import { DepotComponent } from './public/components/depot/depot.component';
-import { DemoOngletComponent } from './public/components/demo-onglet/demo-onglet.component';
-import { Enfant1Component } from './public/components/enfant1/enfant1.component';
-import { Enfant2Component } from './public/components/enfant2/enfant2.component';
-import { LoginComponent } from './public/components/login/login.component';
-import { OnCHangesDemoComponent } from './public/components/on-changes-demo/on-changes-demo.component';
-import { PokemonsComponent } from './public/components/pokemons/pokemons.component';
+import { PublicLayoutComponent } from './public/layout/public-layout/public-layout.component';
 
 export const routes: Routes = [
-  { path: "", redirectTo: "account", pathMatch: "full" },
-  { path: "account", component: AccountComponent, title: "Comptes" },
-  { path: "on-changes", component: OnCHangesDemoComponent, title: "demo onChanges" },
-  { path: "pokemons", component: PokemonsComponent, title: "pokemons" },
   {
-    path: "detail-compte/:id", component: AccountResumeComponent, title: "détail du compte", children: [
-      { path: "", redirectTo: "depot", pathMatch: "full" },
-      { path: "depot", component: DepotComponent },
-      { path: "retrait", component: RetirerComponent }
-    ]
+    path: '',
+    component: PublicLayoutComponent,
+
+    // Charge les routes enfants
+    // ==> Toutes les routes 'enfants' sont les routes de l'espace public
+    loadChildren: () => import('./public/routes/public.routes').then(r => r.PUBLIC_ROUTES),
+
   },
 
-  // Pour lazyloading sur la route "demo-onglets""
+  // REMPLACER PAR LE MODULE ADMIN
   {
-    path: "demo-onglets",
-    loadComponent: () => import('./public/components/demo-onglet/demo-onglet.component').then(m => m.DemoOngletComponent),
-    children: [
-      { path: "depot", component: Enfant1Component },
-      { path: "retrait", component: Enfant2Component },
-    ]
-  },
-  { path: "login", component: LoginComponent, title: "login" },
+    path: 'admin',
 
+    // Avec 'component' , import classique (donc pas de lazyLoading)
+    // component: PublicLayoutComponent,
 
-  { path: "**", redirectTo: "account" },
+    // Pour faire le lazy loading, nous devons utiliser 'loadComponent' à la place de 'component'
+    loadComponent: () => import('./admin/layout/admin-layout/admin-layout.component').then(c => c.AdminLayoutComponent),
+
+    // Charge les routes enfants
+    // ==> Toutes les routes 'enfants' sont les routes de l'espace public
+    loadChildren: () => import('./admin/routes/admin.route').then(r => r.ADMIN_ROUTES)
+  }
 ];
---
+++ src/app/public/routes/public.routes.ts
@@ -0,0 +1,37 @@
+import { Routes } from "@angular/router";
+import { AccountComponent } from "../components/account/account.component";
+import { OnCHangesDemoComponent } from "../components/on-changes-demo/on-changes-demo.component";
+import { PokemonsComponent } from "../components/pokemons/pokemons.component";
+import { AccountResumeComponent } from "../components/account-resume/account-resume.component";
+import { DepotComponent } from "../components/depot/depot.component";
+import { RetirerComponent } from "../components/retirer/retirer.component";
+import { Enfant1Component } from "../components/enfant1/enfant1.component";
+import { Enfant2Component } from "../components/enfant2/enfant2.component";
+import { LoginComponent } from "../components/login/login.component";
+
+export const PUBLIC_ROUTES: Routes = [
+  { path: "", redirectTo: "account", pathMatch: "full" },
+  { path: "account", component: AccountComponent, title: "Comptes" },
+  { path: "on-changes", component: OnCHangesDemoComponent, title: "demo onChanges" },
+  { path: "pokemons", component: PokemonsComponent, title: "pokemons" },
+  {
+    path: "detail-compte/:id", component: AccountResumeComponent, title: "détail du compte", children: [
+      { path: "", redirectTo: "depot", pathMatch: "full" },
+      { path: "depot", component: DepotComponent },
+      { path: "retrait", component: RetirerComponent }
+    ]
+  },
+
+  // Pour lazyloading sur la route "demo-onglets""
+  {
+    path: "demo-onglets",
+    loadComponent: () => import('../components/demo-onglet/demo-onglet.component').then(m => m.DemoOngletComponent),
+
+    children: [
+      { path: "depot", component: Enfant1Component },
+      { path: "retrait", component: Enfant2Component },
+    ]
+  },
+  { path: "login", component: LoginComponent, title: "login" },
+  { path: "**", redirectTo: "account" },
+]
